7550a26cfeccc1f415f40d7f1381f134c931a0ec
Farewe1ll <Farewe1ll_youngjh@foxmail.com>
2025-07-30 03:21:00 +0800

    fixed actions

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -115,13 +115,13 @@ export async function getCategoryList(): Promise<Category[]> {
 
 export async function getPostSeries(
 	seriesName: string,
-): Promise<{ body: string; data: BlogPostData; slug: string }[]> {
+): Promise<{ body: string; data: CollectionEntry<"posts">["data"]; slug: string }[]> {
 	const posts = (await getCollection("posts", ({ data }) => {
 		return (
 			(import.meta.env.PROD ? data.draft !== true : true) &&
 			data.series === seriesName
 		);
-	})) as unknown as { body: string; data: BlogPostData; slug: string }[];
+	})) as unknown as { body: string; data: CollectionEntry<"posts">["data"]; slug: string }[];
 
 	posts.sort((a, b) => {
 		const dateA = new Date(a.data.published);
