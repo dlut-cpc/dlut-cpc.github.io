a375fad8c97a7bd053f115fee7d1318e454d1b14
Farewe1ll <Farewe1ll_youngjh@foxmail.com>
2025-07-30 03:22:36 +0800

    fixed biome

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -115,13 +115,19 @@ export async function getCategoryList(): Promise<Category[]> {
 
 export async function getPostSeries(
 	seriesName: string,
-): Promise<{ body: string; data: CollectionEntry<"posts">["data"]; slug: string }[]> {
+): Promise<
+	{ body: string; data: CollectionEntry<"posts">["data"]; slug: string }[]
+> {
 	const posts = (await getCollection("posts", ({ data }) => {
 		return (
 			(import.meta.env.PROD ? data.draft !== true : true) &&
 			data.series === seriesName
 		);
-	})) as unknown as { body: string; data: CollectionEntry<"posts">["data"]; slug: string }[];
+	})) as unknown as {
+		body: string;
+		data: CollectionEntry<"posts">["data"];
+		slug: string;
+	}[];
 
 	posts.sort((a, b) => {
 		const dateA = new Date(a.data.published);
